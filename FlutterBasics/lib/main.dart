b4cb2c74fb38b771e9ab6703e5f864181ea00bf3
최서연 <yeondublue@naver.com>
2022-11-19 17:59:36 +0900

    add SingleChildScrollView

--- FlutterBasics/lib/main.dart
+++ FlutterBasics/lib/main.dart
@@ -43,95 +43,104 @@ class CenterText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.pink,
-      child: Column(
-        mainAxisSize: MainAxisSize.max, // 안그러면 길이가 최대로 잡힘, max도 마찬가지
-        mainAxisAlignment: MainAxisAlignment.center, // y축
-        crossAxisAlignment: CrossAxisAlignment.end, // x축
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Expanded(
-                flex: 1, // 비율을 의미, flex를 두개를 10, 10 주면 50%, 50% 차지
-                child: Text("1. row1 veryverylong sentences..........."),
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisSize: MainAxisSize.max, // 안그러면 길이가 최대로 잡힘, max도 마찬가지
+          crossAxisAlignment: CrossAxisAlignment.center, // x축
+          children: [
+            const Expanded(child: Text("Last"),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Expanded(
+                  flex: 1, // 비율을 의미, flex를 두개를 10, 10 주면 50%, 50% 차지
+                  child: Text("1. row1 veryverylong sentences..........."),
+                ),
+                Container(
+                  height: 20,
+                  width: 20,
+                  color: Colors.yellow,
+                ),
+                const Flexible(
+                  // flex 값으로 지정한 비율 내에서, Expanded를 쓰면 남은 영역 모두 차지, row는 기본적으로 한 줄에 다 보여주려 함
+                  child: Text(
+                    "2. row2 veryverylong sentences...........",
+                    maxLines: 2, // 2줄만 표시
+                    overflow: TextOverflow.ellipsis,
+                  ), // flexible을 줘서 여러줄 표시 가능
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Colors.green,
+                )
+              ],
+            ),
+            Container(
+              color: Colors.green,
+              child: const Padding(
+                // refactor로 padding 손쉽게 넣을 수 있음
+                padding: EdgeInsets.all(16.0),
+                child: Text("First Hello, world!"),
               ),
-              Container(
-                height: 20,
-                width: 20,
-                color: Colors.yellow,
+            ),
+            const SizedBox(
+              // swiftui spacer 사이즈 준 거랑 비슷한 듯
+              height: 30,
+              width: double.infinity,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.blue,
               ),
-              const Flexible(
-                // flex 값으로 지정한 비율 내에서, Expanded를 쓰면 남은 영역 모두 차지, row는 기본적으로 한 줄에 다 보여주려 함
+              width: 150,
+              height: 150,
+              child: const Center(
                 child: Text(
-                  "2. row2 veryverylong sentences...........",
-                  maxLines: 2, // 2줄만 표시
-                  overflow: TextOverflow.ellipsis,
-                ), // flexible을 줘서 여러줄 표시 가능
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                color: Colors.green,
-              )
-            ],
-          ),
-          Container(
-            color: Colors.green,
-            child: const Padding(
-              // refactor로 padding 손쉽게 넣을 수 있음
-              padding: EdgeInsets.all(16.0),
-              child: Text("First Hello, world!"),
-            ),
-          ),
-          const SizedBox(
-            // swiftui spacer 사이즈 준 거랑 비슷한 듯
-            height: 30,
-            width: double.infinity,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.blue,
-            ),
-            width: 150,
-            height: 150,
-            child: const Center(
-              child: Text(
-                "Hello",
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 20,
+                  "Hello",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 16,),
-          const SizedBox(
-            width: 100,
-            height: 100,
-            child: CircleAvatar(
-                backgroundImage: NetworkImage("https://picsum.photos/200")),
-          ),
-          Stack( // swiftui의 zstack 비슷한 듯
-            children: [
-              SizedBox(
-                width: 100,
-                height: 150,
-                child: Image.asset(
-                  'assets/logo.jpeg',
-                  fit: BoxFit.cover,
+            const SizedBox(height: 16,),
+            const SizedBox(
+              width: 100,
+              height: 100,
+              child: CircleAvatar(
+                  backgroundImage: NetworkImage("https://picsum.photos/200")),
+            ),
+            Stack( // swiftui의 zstack 비슷한 듯
+              children: [
+                SizedBox(
+                  width: 100,
+                  height: 150,
+                  child: Image.asset(
+                    'assets/logo.jpeg',
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              const Positioned(
-                top: 130,
-                child: Text("Logo"),
-              ) 
-            ],
-          ),
-        ],
+                const Positioned(
+                  top: 130,
+                  child: Text("Logo"),
+                ),
+                Container(
+                  width: 100,
+                  height: 500,
+                  color: Colors.green,
+                  child: const Text("Last Text"),
+                ) 
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -176,3 +185,6 @@ class CenterText extends StatelessWidget {
 // row: horizental 표시, scroll 불가
 // flexible: 여러 flexible 위젯이 있으면 flex 값을 줘서 비율 정하기 가능
 // expanded: flex 값 내에서 허용된 너비를 모두 사용
+
+// assets: pubspec.yaml에서 추가 가능
+// pubspec.yaml 수정 시, flutter pub get 명령어 수행
